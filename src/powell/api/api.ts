b04d38d914eecffdd94d6057d0bac6131ccd51a6
Your Name <you@example.com>
2024-07-24 15:36:27 +0330

    update prime api

--- src/powell/api/api.ts
+++ src/powell/api/api.ts
@@ -1,9 +1,44 @@
-import { Button, ButtonProps } from "primereact/button";
-import { classNames, UniqueComponentId } from "primereact/utils";
-import { InputText, InputTextProps } from "primereact/inputtext";
+import {
+  AutoComplete,
+  AutoCompleteChangeEvent,
+  AutoCompleteCompleteEvent,
+  AutoCompleteDropdownClickEvent,
+  AutoCompleteProps,
+  AutoCompleteSelectEvent,
+  AutoCompleteUnselectEvent,
+} from "primereact/autocomplete";
+import {
+  Calendar,
+  CalendarDateTemplateEvent,
+  CalendarMonthChangeEvent,
+  CalendarMonthNavigatorTemplateEvent,
+  CalendarNavigatorTemplateEvent,
+  CalendarProps,
+  CalendarSelectEvent,
+  CalendarViewChangeEvent,
+  CalendarVisibleChangeEvent,
+  CalendarYearNavigatorTemplateEvent,
+} from "primereact/calendar";
+import {
+  Checkbox,
+  CheckboxChangeEvent,
+  CheckboxContext,
+  CheckboxPassThroughMethodOptions,
+  CheckboxPassThroughOptions,
+  CheckboxPassThroughType,
+  CheckboxProps,
+  CheckboxState
+} from "primereact/checkbox";
+import { Dropdown, DropdownChangeEvent, DropdownFilterEvent, DropdownProps } from "primereact/dropdown";
 import { FloatLabel } from "primereact/floatlabel";
+import { InputIcon } from 'primereact/inputicon';
 import { IconField, IconFieldProps } from "primereact/iconfield";
-import { InputIcon } from "primereact/inputicon";
+import { InputText, InputTextProps } from "primereact/inputtext";
+import { InputTextarea, InputTextareaProps } from "primereact/inputtextarea";
+import { Button, ButtonProps } from "primereact/button";
+import { Sidebar } from "primereact/sidebar";
+import { PanelMenu } from 'primereact/panelmenu';
+import { MenuItem } from "primereact/menuitem";
 import {
   addLocale,
   APIOptions,
@@ -16,32 +51,71 @@ import {
   updateLocaleOptions,
   ZIndexOptions
 } from "primereact/api";
-import { PanelMenu } from 'primereact/panelmenu';
-import { Sidebar } from "primereact/sidebar";
-import { MenuItem } from "primereact/menuitem";
-import { InputTextarea, InputTextareaProps } from "primereact/inputtextarea";
-import { Dropdown, DropdownChangeEvent, DropdownProps } from "primereact/dropdown";
+import { classNames, UniqueComponentId } from "primereact/utils";
+
+export const PrimeAutoComplete = AutoComplete;
+export type PrimeAutoCompleteProps = AutoCompleteProps;
+export type PrimeAutoCompleteSelectEvent = AutoCompleteSelectEvent;
+export type PrimeAutoCompleteUnselectEvent = AutoCompleteUnselectEvent;
+export type PrimeAutoCompleteCompleteEvent = AutoCompleteCompleteEvent;
+export type PrimeAutoCompleteChangeEvent = AutoCompleteChangeEvent;
+export type PrimeAutoCompleteDropdownClickEvent = AutoCompleteDropdownClickEvent;
+
+export const PrimeCalendar = Calendar;
+export type PrimeCalendarProps = CalendarProps;
+export type PrimeCalendarSelectEvent = CalendarSelectEvent;
+export type PrimeCalendarNavigatorTemplateEvent = CalendarNavigatorTemplateEvent;
+export type PrimeCalendarDateTemplateEvent = CalendarDateTemplateEvent;
+export type PrimeCalendarMonthChangeEvent = CalendarMonthChangeEvent;
+export type PrimeCalendarVisibleChangeEvent = CalendarVisibleChangeEvent;
+export type PrimeCalendarViewChangeEvent = CalendarViewChangeEvent;
+export type PrimeCalendarMonthNavigatorTemplateEvent = CalendarMonthNavigatorTemplateEvent;
+export type PrimeCalendarYearNavigatorTemplateEvent = CalendarYearNavigatorTemplateEvent;
+
+export const PrimeCheckbox = Checkbox;
+export type PrimeCheckboxChangeEvent = CheckboxChangeEvent;
+export type PrimeCheckboxPassThroughMethodOptions = CheckboxPassThroughMethodOptions;
+export type PrimeCheckboxPassThroughType<T> = CheckboxPassThroughType<T>;
+export type PrimeCheckboxPassThroughOptions = CheckboxPassThroughOptions;
+export type PrimeCheckboxProps = CheckboxProps;
+export type PrimeCheckboxState = CheckboxState;
+export type PrimeCheckboxContext = CheckboxContext;
 
 export const PrimeButton = Button;
 export type PrimeButtonProps = ButtonProps;
+
 export const PrimeInputText = InputText;
-export const PrimeInputTextarea = InputTextarea;
 export type PrimeInputTextProps = InputTextProps;
+
+export const PrimeInputTextarea = InputTextarea;
 export type PrimeInputTextareaProps = InputTextareaProps;
-export const PrimeFloatLabel = FloatLabel;
+
 export const PrimeIconField = IconField;
 export type PrimeIconFieldProps = IconFieldProps;
+
+export const PrimeFloatLabel = FloatLabel;
+
 export const PrimeInputIcon = InputIcon;
+
 export type PrimeAPIOptions = APIOptions;
-export type PrimeDropdownChangeEvent = DropdownChangeEvent;
-export const PrimeProvider = PrimeReactProvider;
-export const PrimeContext = PrimeReactContext;
-export const PrimePanelMenu = PanelMenu;
-export const PrimeSidebar = Sidebar;
+
 export const PrimeDropdown = Dropdown;
 export type PrimeDropdownProps = DropdownProps;
+export type PrimeDropdownChangeEvent = DropdownChangeEvent;
+export type PrimeDropdownFilterEvent = DropdownFilterEvent;
+
+export const PrimeProvider = PrimeReactProvider;
+
+export const PrimeContext = PrimeReactContext;
+
+export const PrimePanelMenu = PanelMenu;
+
+export const PrimeSidebar = Sidebar;
+
 export type PrimeMenuItem = MenuItem;
+
 export type PrimeZIndexOptions = ZIndexOptions;
+
 export const primeLocale = locale;
 export const primeAddLocale = addLocale;
 export const primeUpdateLocaleOption = updateLocaleOption;
